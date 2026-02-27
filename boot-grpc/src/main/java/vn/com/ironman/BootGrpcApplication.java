cc7015b707064998d49d56f7c6d6e44036a05c45
phutg <phutg@unit.com.vn>
2026-02-27 21:43:25 +0700

    initialize Spring Boot gRPC application with Maven wrapper and configuration files

---
+++ boot-grpc/src/main/java/vn/com/ironman/BootGrpcApplication.java
@@ -0,0 +1,13 @@
+package vn.com.ironman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGrpcApplication.class, args);
+	}
+
+}
---
+++ boot-grpc/src/test/java/vn/com/ironman/BootGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.ironman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
